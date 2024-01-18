f815b4295e37f7224c79fcf551b8b4afdd7ac1e9
Aris <72753404+aristsam@users.noreply.github.com>
2024-01-18 23:07:06 +0200

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -28,7 +28,7 @@ include "../config.php";
       <!-- Sidebar -->
       <ul class="navbar-nav bg-dark sidebar sidebar-dark accordion" id="accordionSidebar">
          <!-- Sidebar - Brand -->
-         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="http://localhost/blog/index.php">
+         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../index.php">
             <div class="sidebar-brand-icon rotate-n-15"> <i class="fa fa-smile-o"></i> </div>
             <div class="sidebar-brand-text mx-3">Blog</div>
          </a>
